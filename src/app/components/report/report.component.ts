201a061ff01783343c00ee8cef02a31643641862
Marcelo Pilatti <marcelo.silva979@gmail.com>
2019-10-16 07:42:59 -0300

    Report page refactoring

--- src/app/components/report/report.component.ts
+++ src/app/components/report/report.component.ts
@@ -12,12 +12,16 @@ import { Vision } from 'src/app/models/vision.model';
 
 import { Legend } from 'src/app/models/legend.model';
 
+import { ReportService } from 'src/app/services/report.service';
+
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
   styleUrls: ['./report.component.css']
 })
 export class ReportComponent implements OnInit {
+
+  private reportConfig;
 
   carRegister: string;
 
@@ -26,8 +30,6 @@ export class ReportComponent implements OnInit {
   bbox: string;
 
   cityBBox: string;
-
-  private reportConfig;
 
   visions: Vision[] = [];
 
@@ -54,7 +56,8 @@ export class ReportComponent implements OnInit {
   constructor(
     private activatedRoute: ActivatedRoute,
     private hTTPService: HTTPService,
-    private configService: ConfigService
+    private configService: ConfigService,
+    private reportService: ReportService
   ) { }
 
   ngOnInit() {
@@ -70,244 +73,35 @@ export class ReportComponent implements OnInit {
     const viewId = propertyConfig.viewId;
     const carRegister = this.carRegister;
     this.hTTPService.get(url, {viewId, carRegister}).subscribe((propertyData: Property) => {
+      const burnedAreas = propertyData.burnedAreas;
+
+      const area = propertyData.area;
+
       const bboxArray = propertyData.bbox.split(',');
       this.bbox = bboxArray[0].split(' ').join(',') + ',' + bboxArray[1].split(' ').join(',');
 
-      const cityBBoxArray = propertyData.cityBBox.split(',');
+      const cityBBoxArray = propertyData.citybbox.split(',');
       this.cityBBox = cityBBoxArray[0].split(' ').join(',') + ',' + cityBBoxArray[1].split(' ').join(',');
 
       this.property = propertyData;
 
-      this.getVisions();
+      this.visions = this.reportService.getVisions(propertyData, this.reportConfig, this.bbox, this.cityBBox);
 
-      this.getDetailedVisions();
+      this.detailedVisions = this.reportService.getDetailedVisions(propertyData, this.reportConfig, this.bbox, this.cityBBox);
 
-      this.getDeforestations();
+      this.deforestations = this.reportService.getDeforestations(propertyData, this.reportConfig, this.bbox);
 
-      this.getDeforestationHistories();
+      this.deforestationHistories = this.reportService.getDeforestationHistories(propertyData, this.reportConfig, this.bbox);
 
-      this.getBurningSpotlightsChart(propertyData.burningSpotlights);
+      this.burningSpotlightsChartData = this.reportService.getBurningSpotlightsChart(propertyData.burningSpotlights);
 
-      this.getBurnedAreas();
+      this.burnedAreas = this.reportService.getBurnedAreas(propertyData, this.reportConfig, this.bbox);
 
-      this.getBurnedAreasChart(propertyData.burnedAreas, propertyData.area);
+      this.burnedAreasChartData = this.reportService.getBurnedAreasChart(burnedAreas);
 
-      this.getLandsatHistories();
-    });
-  }
+      this.burnedAreasPerPropertyChartDatas = this.reportService.getBurnedAreasPerPropertyChart(burnedAreas, area);
 
-  getVisions() {
-    const visionsData = this.reportConfig.visions;
-    visionsData.forEach((visionData: Vision) => {
-      let image = visionData.image;
-      image = image.replace('{bbox}', this.bbox);
-      image = image.replace('{citybbox}', this.cityBBox);
-      image = image.replace('{cityCqlFilter}', `municipio='${this.property.city}';numero_do2='${this.property.register}'`);
-      image = image.replace('{propertyCqlFilter}', `numero_do2='${this.property.register}'`);
-      image = image.replace(/\{carRegister\}/g, `'${this.property.register}'`);
-      const currentDateInput = JSON.parse(localStorage.getItem('dateFilter'));
-      image = image.replace('{date}', `${currentDateInput[0]}/${currentDateInput[1]}`);
-
-      const vision = new Vision(
-        visionData.id,
-        visionData.title,
-        image,
-        visionData.description,
-        visionData.layerData
-      );
-      this.visions.push(vision);
-    });
-  }
-
-  getDetailedVisions() {
-    const detailedVisionsData = this.reportConfig.detailedVisions;
-    detailedVisionsData.forEach((detailedVisionData: Vision) => {
-      let image = detailedVisionData.image;
-      image = image.replace('{bbox}', this.bbox);
-      const currentDateInput = JSON.parse(localStorage.getItem('dateFilter'));
-      image = image.replace('{date}', `${currentDateInput[0]}/${currentDateInput[1]}`);
-      image = image.replace('{propertyCqlFilter}', `numero_do2='${this.property.register}'`);
-      image = image.replace(/\{carRegister\}/g, `'${this.property.register}'`);
-      const vision = new Vision(
-        detailedVisionData.id,
-        detailedVisionData.title,
-        image,
-        detailedVisionData.description,
-        detailedVisionData.layerData
-      );
-      this.detailedVisions.push(vision);
-    });
-  }
-
-  getDeforestations() {
-    const deforestationsData = this.reportConfig.deforestations;
-    deforestationsData.forEach((deforestationData: Vision) => {
-      let image = deforestationData.image;
-      image = image.replace('{bbox}', this.bbox);
-      image = image.replace('{propertyCqlFilter}', `numero_do2='${this.property.register}'`);
-      const currentDateInput = JSON.parse(localStorage.getItem('dateFilter'));
-      image = image.replace('{date}', `${currentDateInput[0]}/${currentDateInput[1]}`);
-      const vision = new Vision(
-        deforestationData.id,
-        deforestationData.title,
-        image,
-        deforestationData.description,
-        deforestationData.layerData
-      );
-      this.deforestations.push(vision);
-    });
-  }
-
-  getDeforestationHistories() {
-    const deforestationHistoriesData = this.reportConfig.deforestationHistories;
-    deforestationHistoriesData.forEach((deforestationHistoryData: Vision) => {
-      let image = deforestationHistoryData.image;
-      image = image.replace('{bbox}', this.bbox);
-      image = image.replace('{propertyCqlFilter}', `numero_do2='${this.property.register}'`);
-      const currentDateInput = JSON.parse(localStorage.getItem('dateFilter'));
-      image = image.replace('{date}', `${currentDateInput[0]}/${currentDateInput[1]}`);
-      const vision = new Vision(
-        deforestationHistoryData.id,
-        deforestationHistoryData.title,
-        image,
-        deforestationHistoryData.description,
-        deforestationHistoryData.layerData
-      );
-      this.deforestationHistories.push(vision);
-    });
-  }
-
-  getBurningSpotlights() {
-    const burningSpotlightsData = this.reportConfig.burningSpotlights;
-    burningSpotlightsData.forEach((burningSpotlightData: Vision) => {
-      let image = burningSpotlightData.image;
-      image = image.replace('{bbox}', this.bbox);
-      image = image.replace('{propertyCqlFilter}', `numero_do2='${this.property.register}'`);
-      const currentDateInput = JSON.parse(localStorage.getItem('dateFilter'));
-      image = image.replace('{date}', `${currentDateInput[0]}/${currentDateInput[1]}`);
-      const vision = new Vision(
-        burningSpotlightData.id,
-        burningSpotlightData.title,
-        image,
-        burningSpotlightData.description,
-        burningSpotlightData.layerData
-      );
-      this.burningSpotlights.push(vision);
-    });
-  }
-
-  getBurningSpotlightsChart(burningSpotlightsData) {
-    const burningSpotlightsYears = [];
-    const burningSpotlights = [];
-    burningSpotlightsData.forEach(burningSpotlightData => {
-      const focusCount = burningSpotlightData['focuscount'];
-      const year = burningSpotlightData['year'];
-      burningSpotlightsYears.push(year);
-      burningSpotlights.push(focusCount);
-    });
-
-    this.burningSpotlightsChartData = {
-      labels: burningSpotlightsYears,
-      datasets: [
-          {
-              label: 'Focos',
-              backgroundColor: [
-                '#4BC0C0',
-                '#FFCE56',
-                '#aa7900',
-                '#36A2EB',
-                '#FF6384'
-              ],
-              data: burningSpotlights
-          }
-      ]
-    };
-  }
-
-  getBurnedAreas() {
-    const burnedAreasData = this.reportConfig.burnedAreas;
-    burnedAreasData.forEach((burnedAreaData: Vision) => {
-      let image = burnedAreaData.image;
-      image = image.replace('{bbox}', this.bbox);
-      image = image.replace('{propertyCqlFilter}', `numero_do2='${this.property.register}'`);
-      const currentDateInput = JSON.parse(localStorage.getItem('dateFilter'));
-      image = image.replace('{date}', `${currentDateInput[0]}/${currentDateInput[1]}`);
-      const vision = new Vision(
-        burnedAreaData.id,
-        burnedAreaData.title,
-        image,
-        burnedAreaData.description,
-        burnedAreaData.layerData
-      );
-      this.burnedAreas.push(vision);
-    });
-  }
-
-  getBurnedAreasChart(burnedAreasData, propertyArea) {
-    const burnedAreasYears = [];
-    const burnedAreas = [];
-    const burnedAreasPerProperty = [];
-    burnedAreasData.forEach(burnedAreaData => {
-      const focusCount = burnedAreaData['focuscount'];
-      const year = burnedAreaData['year'];
-      burnedAreasYears.push(year);
-      burnedAreas.push(focusCount);
-      burnedAreasPerProperty.push([propertyArea, focusCount]);
-    });
-
-    this.burnedAreasChartData = {
-      labels: burnedAreasYears,
-      datasets: [
-          {
-              label: 'Focos',
-              backgroundColor: [
-                '#FF6384',
-                '#4BC0C0',
-                '#FFCE56',
-                '#aa7900',
-                '#36A2EB'
-              ],
-              data: burnedAreas
-          }
-      ]
-    };
-
-    burnedAreasPerProperty.forEach(burnedArea => {
-      const chartData = {
-        labels: ['Área imóvel', 'Área queimada'],
-        datasets: [
-            {
-                backgroundColor: [
-                  '#FF6384',
-                  '#4BC0C0',
-                  '#FFCE56',
-                  '#aa7900',
-                  '#36A2EB'
-                ],
-                data: burnedArea
-            }
-        ]
-      };
-      this.burnedAreasPerPropertyChartDatas.push(chartData);
-    });
-  }
-
-  getLandsatHistories() {
-    const landsatHistoriesData = this.reportConfig.landsatHistories;
-    landsatHistoriesData.forEach((landsatHistoryData: Vision) => {
-      let image = landsatHistoryData.image;
-      image = image.replace('{bbox}', this.bbox);
-      image = image.replace('{propertyCqlFilter}', `numero_do2='${this.property.register}'`);
-      const currentDateInput = JSON.parse(localStorage.getItem('dateFilter'));
-      image = image.replace('{date}', `${currentDateInput[0]}/${currentDateInput[1]}`);
-      const vision = new Vision(
-        landsatHistoryData.id,
-        landsatHistoryData.title,
-        image,
-        landsatHistoryData.description,
-        landsatHistoryData.layerData
-      );
-      this.landsatHistories.push(vision);
+      this.landsatHistories = this.reportService.getLandsatHistories(propertyData, this.reportConfig, this.bbox);
     });
   }
 }
--- src/app/models/property.model.ts
+++ src/app/models/property.model.ts
@@ -4,7 +4,7 @@ export class Property {
     public area: number,
     public name: string,
     public city: string,
-    public cityBBox: string,
+    public citybbox: string,
     public bbox: string,
     public latLong: string[],
     public burningSpotlights?: [],
