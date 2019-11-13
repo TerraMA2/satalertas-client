97b71a0b11915765427282f05780876dbe630fee
Marcelo Pilatti <marcelo.silva979@gmail.com>
2019-11-13 16:49:47 -0300

    Fixes on the final report

--- src/app/components/final-report/final-report.component.ts
+++ src/app/components/final-report/final-report.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Report } from 'src/app/models/report.model';
-
 import { ActivatedRoute } from '@angular/router';
 
 import { HTTPService } from 'src/app/services/http.service';
 
 import { ConfigService } from 'src/app/services/config.service';
+
 import { Property } from 'src/app/models/property.model';
 
 @Component({
@@ -18,9 +17,20 @@ export class FinalReportComponent implements OnInit {
 
   private reportConfig;
 
-  report: Report;
+  property: Property;
 
   carRegister: string;
+
+  dateFilter: string;
+
+  formattedFilterDate: string;
+
+  currentYear: number;
+  prodesStartYear: string;
+
+  tableColumns;
+
+  tableData;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -31,27 +41,56 @@ export class FinalReportComponent implements OnInit {
   ngOnInit() {
     this.activatedRoute.params.subscribe(params => this.carRegister = params.carRegister);
     this.reportConfig = this.configService.getReportConfig();
+
+    this.tableColumns = [
+      { field: 'affectedArea', header: 'Área atingida' },
+      { field: 'recentDeforestation', header: 'Desmatamento recente (DETER – nº de alertas)' },
+      { field: 'pastDeforestation', header: 'Desmatamento pretérito (PRODES – ha ano-1)' }
+    ];
+
+    this.tableData = [
+      { affectedArea: 'APP', recentDeforestation: '', pastDeforestation: '' },
+      { affectedArea: 'ARL', recentDeforestation: '', pastDeforestation: '' },
+      { affectedArea: 'AUR', recentDeforestation: '', pastDeforestation: '' },
+      { affectedArea: 'UC - PI', recentDeforestation: '', pastDeforestation: '' },
+      { affectedArea: 'UC - US', recentDeforestation: '', pastDeforestation: '' },
+      { affectedArea: 'TI', recentDeforestation: '', pastDeforestation: '' },
+      { affectedArea: 'AUC', recentDeforestation: '', pastDeforestation: '' },
+      { affectedArea: 'AUAS', recentDeforestation: '', pastDeforestation: '' },
+      { affectedArea: 'AUTEX', recentDeforestation: '', pastDeforestation: '' },
+      { affectedArea: 'AD', recentDeforestation: '', pastDeforestation: '' },
+      { affectedArea: 'Área autuada', recentDeforestation: '', pastDeforestation: '' },
+      { affectedArea: 'Área embargada', recentDeforestation: '', pastDeforestation: '' },
+      { affectedArea: 'Área desembargada', recentDeforestation: '', pastDeforestation: '' }
+    ];
+
     this.getReportData();
   }
 
   getReportData() {
     const date = JSON.parse(localStorage.getItem('dateFilter'));
+    const startDate = new Date(date[0]).toLocaleDateString('pt-BR');
+    const endDate = new Date(date[1]).toLocaleDateString('pt-BR');
+
+    this.currentYear = new Date().getFullYear();
+
+    this.formattedFilterDate = `${startDate} - ${endDate}`;
+
+    this.dateFilter = `${date[0]}/${date[1]}`;
     const propertyConfig = this.reportConfig.propertyData;
     const url = propertyConfig.url;
     const viewId = propertyConfig.viewId;
     this.carRegister = this.carRegister.replace('\\', '/');
     const carRegister = this.carRegister;
     this.hTTPService.get(url, {viewId, carRegister, date}).subscribe((reportData: Property) => {
-        this.report = new Report('Relatório',
-                                reportData.city,
-                                'Promotoria de Justiça de Sinop',
-                                '00001',
-                                reportData.name,
-                                '000.000.000-00',
-                                this.carRegister,
-                                reportData['prodesArea'],
-                                date
-        );
+      this.prodesStartYear = reportData.prodesYear[0]['date'];
+
+      const bboxArray = reportData.bbox.split(',');
+      const bbox = bboxArray[0].split(' ').join(',') + ',' + bboxArray[1].split(' ').join(',');
+
+      reportData.bbox = bbox;
+      this.property = reportData;
+
     });
   }
 
