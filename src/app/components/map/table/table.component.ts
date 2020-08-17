44fe384c56d67c4f188e27b0ddc9bb8c13acbb07
Miguel Thiago <miguelthiago.ufla@gmail.com>
2020-08-17 15:25:51 -0300

    Standardized View sent to the export method

--- src/app/components/map/table/table.component.ts
+++ src/app/components/map/table/table.component.ts
@@ -329,12 +329,12 @@ export class TableComponent implements OnInit {
 
         const view = new View(
             layer.value,
-            layer.cod,
-            layer.codgroup,
-            (layer.type === LayerType.ANALYSIS),
-            layer.isPrimary,
-            layer.tableOwner,
-            layer.tableName
+            layer.label.toUpperCase().replace(' ', '_'),
+            layer.cod_group,
+          layer.cod_group !== 'CAR',
+            true,
+            layer.table_name,
+            layer.table_name
         );
 
         const params = await this.filterService.getParams(view);
