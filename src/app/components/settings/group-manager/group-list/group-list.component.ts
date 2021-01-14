7f3d7bcb4ff892de961bfdd33aa156d8dd6b86bd
kylefelipe <kylefelipe@gmail.com>
2021-01-13 22:17:19 -0300

    Create Group manager

--- src/app/components/settings/group-manager/group-list/group-list.component.ts
+++ src/app/components/settings/group-manager/group-list/group-list.component.ts
@@ -1,53 +1,119 @@
-import { SettingsService } from 'src/app/services/settings.service';
+import { SettingsService } from 'src/app/services/group.service';
 import { Component, OnInit } from '@angular/core';
-import {TreeNode, MessageService} from 'primeng-lts/api';
-import { Evented } from 'leaflet';
-
+import {TreeNode, MessageService, ConfirmationService} from 'primeng-lts/api';
+import { Group } from '../../../../models/groups.model';
+import ConfigJson from 'src/assets/config.json';
 @Component({
   selector: 'app-group-list',
   templateUrl: './group-list.component.html',
   styleUrls: ['./group-list.component.css'],
-  providers: [MessageService]
+  providers: [MessageService, ConfirmationService]
 })
 
 export class GroupListComponent implements OnInit {
+  confButtons;
+  idCompany;
+  groups: Group[] = [];
+  confModalButtons;
+  lines = [];
   selectedGroups: TreeNode[];
-
-  files: TreeNode[];
-
-  cols: any[] = [];
-
+  files: any[];
+  cols = ConfigJson.settings.groupsColumns;
   dialogVisible: boolean;
-
+  inputs = [];
+  submitted: boolean;
+  group: Group = {};
 
   constructor(
     private settingsService: SettingsService,
-    private messageService: MessageService
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService
   ) { }
 
-  ngOnInit(): void {
-    this.settingsService.getFiles().then(res => {
-      this.files = res.data;
-      this.cols = res.cols;
+  async ngOnInit() {
+    await this.settingsService.getAllGroups().then(res => {
+      this.groups = res;
+      for (let field of this.cols) {
+        const fiel = {
+          show: true,
+          disable: false,
+          label: `${field.header}`,
+          inputClass: 'input-group',
+          type: 'text',
+          // model: '',
+          onChange: (event) => { console.log(event)},
+          field: `${field.field}`,
+          required: true
+        };
+        this.inputs.push(fiel);
+      };
+      this.lines = [
+        { show: true,
+          label: '',
+          inputs: [...this.inputs]}
+      ];
     });
   }
 
-  groupSelect(event) {
-    this.messageService.add({
-      severity: 'info',
-      summary: 'Grupo Selecionado',
-      detail: event.node.data.name
-    });
-    console.log(this.selectedGroups);
-
+  startNewGroup() {
+    this.group = {};
+    this.submitted = false;
+    this.dialogVisible = true;
   }
-  
-  groupUnSelect(event) {
-    this.messageService.add({
-      severity: 'info',
-      summary: 'Node Unselected',
-      detail: event.node.data.name
+
+  cancelNewGroup() {
+    this.dialogVisible = false;
+    this.submitted = false;
+  }
+
+  editGroup(editedGroup: Group) {
+    this.group = { ...editedGroup };
+    this.dialogVisible = true;
+  }
+
+  async removeGroup(deletedGroup: Group) {
+    this.confirmationService.confirm({
+      message: `Deseja deletar o grupo ${deletedGroup.name}?`,
+      header: "Atenção!",
+      icon: "pi pi-exclamation-triangle",
+      accept: () => {
+        this.settingsService.removeGroup(deletedGroup.id)
+        .then(() => {
+          const groupPos = this.groups
+          .findIndex(item => item.id === deletedGroup.id);
+          this.groups.splice(groupPos, 1);
+        });
+        this.messageService.add({
+          severity: 'success',
+          summary: "Sucesso",
+          detail: "Grupo deletado",
+          life: 3000
+        });
+      }
     });
-    console.log(this.selectedGroups);
+  }
+
+  async closeModalDialog() {
+    this.dialogVisible = false;
+    if (this.group.id) {
+      await this.settingsService.editGroup(this.group)
+        .then(response => {
+          const groupPos = this.groups
+          .findIndex(item => item.id === this.group.id);
+          this.groups.splice(groupPos, 1, response);
+        });  
+    } else {
+      await this.settingsService.createNewGroup(this.group)
+      .then(response => {
+          this.groups.push(response);
+      });
+    }
+    this.messageService.add({
+      severity: 'success',
+      summary: "Sucesso",
+      detail: "Grupo salvo",
+      life: 3000
+    });
+    this.group = {};
   }
 }
