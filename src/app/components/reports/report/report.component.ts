b83dbe035e385818ea8b5d772783b0a3fed3706e
Kyle Felipe <kylefelipe@gmail.com>
2021-09-30 13:00:48 -0300

    Sends getContextDeforestationAlerts to server

--- src/app/components/reports/report/report.component.ts
+++ src/app/components/reports/report/report.component.ts
@@ -73,105 +73,6 @@ export class ReportComponent implements OnInit {
 		await this.getReportData();
 	}
 
-	async getContextDeflorestationAlerts(deforestationAlerts) {
-		const deforestationAlertsContext = [];
-		const deforestationAlertsCount = deforestationAlerts.length;
-
-		if (deforestationAlerts && deforestationAlertsCount > 0) {
-			let images = [];
-			let titleDate = [];
-			let subTitleArea = [];
-			let startingYear = new Date().getFullYear();
-
-			for (let i = 0; i < deforestationAlertsCount; ++i) {
-				images.push(this.getImageObject(deforestationAlerts[i].urlGsImageBefore, [225, 225], [0, 0, 0, 0], 'left'));
-				images.push(this.getImageObject(deforestationAlerts[i].urlGsImageCurrent, [225, 225], [13, 0, 0, 0], 'right'));
-
-				startingYear = (deforestationAlerts[i].year - 1) < startingYear ? (deforestationAlerts[i].year - 1) : startingYear;
-
-				titleDate.push({
-					text: `Alerta(${ deforestationAlerts[i].date }) - Imagem(${ deforestationAlerts[i].year - 1 })`,
-					fontSize: 8,
-					style: 'body',
-					alignment: 'center'
-				});
-
-				titleDate.push({
-					text: `Alerta(${ deforestationAlerts[i].date }) - Imagem(${ deforestationAlerts[i].date })`,
-					fontSize: 8,
-					style: 'body',
-					alignment: 'center'
-				});
-
-				subTitleArea.push({
-					text: `${ formatNumber(deforestationAlerts[i].area, 'pt-BR', '1.0-4') } ha`,
-					fontSize: 8,
-					style: 'body',
-					alignment: 'center'
-				});
-
-				if (i === 0) {
-					deforestationAlertsContext.push(
-						{
-							text: `Na  figura 3, a seguir, será  representado  o detalhamento  dos  alertas.`,
-							alignment: 'right',
-							margin: [30, 0, 30, 0],
-							style: 'body'
-						}
-					);
-				} else {
-					deforestationAlertsContext.push(
-						{
-							text: '',
-							pageBreak: 'after'
-						}
-					);
-				}
-				deforestationAlertsContext.push(
-					{
-						columns: titleDate,
-						margin: [30, 0, 30, 0]
-					},
-					{
-						columns: images,
-						margin: [30, 0, 30, 0]
-					},
-					{
-						columns: [
-							this.getImageObject(deforestationAlerts[i].urlGsImagePlanetCurrentAndCar, [420, 420], [0, 0], 'center')
-						],
-						margin: [30, 5, 30, 0]
-					},
-					{
-						columns: subTitleArea,
-						margin: [30, 5, 30, 5]
-					}
-				);
-
-				images = [];
-				titleDate = [];
-				subTitleArea = [];
-			}
-
-			deforestationAlertsContext.push({
-				text: [
-					{
-						text: 'Figura 3. ',
-						bold: true
-					},
-					{
-						text: ` Comparativo de imagens de satélite anterior à ${ startingYear } e atual ${ new Date().getFullYear() }.`,
-						bold: false
-					}
-				],
-				margin: [30, 0, 30, 0],
-				alignment: 'center',
-				fontSize: 9
-			});
-		}
-		return deforestationAlertsContext;
-	}
-
 	async getContextDeforestationHistory(deforestationHistory, urlGsDeforestationHistory, urlGsDeforestationHistory1) {
 		const deforestationHistoryContext = [];
 		const deforestationHistoryCount = deforestationHistory.length;
@@ -276,9 +177,7 @@ export class ReportComponent implements OnInit {
 		if (this.reportData['type'] === 'prodes') {
 			this.reportData['deforestationHistoryContext'] = await this.getContextDeforestationHistory(this.reportData.property['deforestationHistory'], this.reportData.urlGsDeforestationHistory, this.reportData.urlGsDeforestationHistory1);
 		}
-		// if (this.reportData['type'] === 'deter') {
-		// 	this.reportData['deforestationAlertsContext'] = await this.getContextDeflorestationAlerts(this.reportData.property.deforestationAlerts);
-		// }
+
 		this.docBase64 = await this.reportService.createPdf(this.reportData).then((response: Response) => response.data);
 	}
 
