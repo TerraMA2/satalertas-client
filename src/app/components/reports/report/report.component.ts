dd5fffaa82179c4e96a1ab13ebb8388b165aa971
Kyle Felipe <kylefelipe@gmail.com>
2021-09-30 19:22:56 -0300

    Moves 	getContextDeforestationHistory to Server

--- src/app/components/reports/report/report.component.ts
+++ src/app/components/reports/report/report.component.ts
@@ -73,100 +73,6 @@ export class ReportComponent implements OnInit {
 		await this.getReportData();
 	}
 
-	async getContextDeforestationHistory(deforestationHistory, urlGsDeforestationHistory, urlGsDeforestationHistory1) {
-		const deforestationHistoryContext = [];
-		const deforestationHistoryCount = deforestationHistory.length;
-
-		if (deforestationHistory && deforestationHistoryCount > 0) {
-			const deforestationData = [];
-			const deforestationColumns = [];
-
-			deforestationHistoryContext.push({
-				text: '',
-				pageBreak: 'after'
-			});
-			deforestationHistoryContext.push({
-				columns: [{
-					text: `O histórico do desmatamento desde ${ deforestationHistory[0].date } pode ser visto na figura 7.`,
-					margin: [30, 0, 30, 15],
-					style: 'bodyIndentFirst'
-				}]
-			});
-			let count = 1;
-			for (let i = 0; i < deforestationHistoryCount; ++i) {
-				count += 1;
-				const view = deforestationHistory[i].date < 2013 ? 'LANDSAT_5_' :
-					deforestationHistory[i].date < 2017 ? 'LANDSAT_8_' :
-						'SENTINEL_2_';
-
-				let url = deforestationHistory[i].date === 2012 ? urlGsDeforestationHistory1 : urlGsDeforestationHistory.replace(new RegExp('#{image}#', ''), `${ view }${ deforestationHistory[i].date }`);
-				url = url.replace(new RegExp('#{year}#', ''), deforestationHistory[i].date);
-
-				deforestationData.push(
-					[
-						{
-							text: `${ deforestationHistory[i].date }`,
-							style: 'body',
-							alignment: 'center'
-						},
-						this.getImageObject(await this.getBaseImageUrl(url), [117, 117], [5, 0], 'center'),
-						{
-							text: `${ deforestationHistory[i].area }`,
-							style: 'body',
-							alignment: 'center'
-						}
-					]
-				);
-			}
-
-			for (let start = 0; start < deforestationData.length; start += 3) {
-				if (start !== 0 && ((start) % 12) === 0) {
-					deforestationColumns.push({
-						text: '',
-						pageBreak: 'after'
-					});
-				}
-				if ((start + 3) < deforestationData.length) {
-					deforestationColumns.push({
-						margin: [30, 0, 30, 0],
-						alignment: 'center',
-						columns: [
-							...deforestationData.slice(start, start + 3)
-						]
-					});
-				} else {
-					deforestationColumns.push({
-						margin: [30, 0, 30, 0],
-						alignment: 'center',
-						columns: [
-							...deforestationData.slice(start),
-						]
-					});
-				}
-			}
-
-			deforestationHistoryContext.push(
-				...deforestationColumns,
-				{
-					text: [
-						{
-							text: 'Figura 7. ',
-							bold: true
-						},
-						{
-							text: ` Histórico de desmatamento do PRODES desde ${ deforestationHistory[0].date }.`,
-							bold: false
-						}
-					],
-					margin: [30, 0, 30, 0],
-					alignment: 'center',
-					fontSize: 9
-				});
-		}
-
-		return deforestationHistoryContext;
-	}
-
 	async getReportData() {
 		this.inputSat = '';
 		this.textAreaComments = '';
@@ -174,9 +80,6 @@ export class ReportComponent implements OnInit {
 		const filter = localStorage.getItem('filterState');
 		const date = JSON.parse(localStorage.getItem('dateFilter'));
 		this.reportData = await this.reportService.getReportCarData(this.carRegister, date, filter, this.type).then((response: Response) => response.data);
-		if (this.reportData['type'] === 'prodes') {
-			this.reportData['deforestationHistoryContext'] = await this.getContextDeforestationHistory(this.reportData.property['deforestationHistory'], this.reportData.urlGsDeforestationHistory, this.reportData.urlGsDeforestationHistory1);
-		}
 
 		this.docBase64 = await this.reportService.createPdf(this.reportData).then((response: Response) => response.data);
 	}
